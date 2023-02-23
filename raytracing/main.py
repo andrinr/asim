b0dfaecbcd4e55a102cfaf3c3bc68f783fdc906c
andrinr <andrinrehmann@gmail.com>
2023-02-23 15:33:57 +0100

    replace with torch.sum()

--- raytracing/main.py
+++ raytracing/main.py
@@ -11,37 +11,33 @@ frustum[..., 1] = torch.linspace(-1, 1, frustum_resolution[1]).view(1, -1)
 frustum[..., 2] = 1
 
 # rays. xzy pos and xyz dir
-rays = torch.zeros((frustum_resolution[0], frustum_resolution[1], 6), dtype=torch.float32)
-rays[..., :3] = eye_pos.view(1, 1, 3)
-rays[..., 3:] = frustum - rays[..., :3]
-print(rays)
+ray_origin = eye_pos.view(1, 1, 3).expand(frustum_resolution[0], frustum_resolution[1], 3)
+ray_direction = frustum - ray_origin
 
 # sphere xyz pos and radius
-sphere = torch.tensor([0, 0, 4, 1], dtype=torch.float32)
+sphere_pos = torch.tensor([0, 0, 3], dtype=torch.float32)
+sphere_radius = 1
 
 # light xyz pos and rgb color
-light = torch.tensor([0, 0, 3, 1, 1, 1], dtype=torch.float32)
+light_pos = torch.tensor([0, 0, 5], dtype=torch.float32)
+light_color = torch.tensor([1, 1, 1], dtype=torch.float32)
 
 # check if ray intersects with object
-def ray_sphere_intersect(rays, sphere):
-    a = torch.tensordot(rays[...,3:], rays[...,3:], dims=([2], [2]))
-    print("a", a)
-    b = 2 * torch.tensordot(rays[..., :3], rays[..., :3] - sphere[:3], dims=([2], [2]))
-    print("b", b)
-    c = torch.dot(sphere[:3], sphere[:3]) +\
-        torch.tensordot(rays[..., :3], rays[..., :3], dims=([2], [2])) -\
-        2 * torch.tensordot(sphere[:3], rays[..., :3]) -\
-        sphere[3] ** 2
-    print("c", c)
+def ray_sphere_intersect(ray_origin, ray_direction, sphere_pos, sphere_radius):
+    a = torch.sum(ray_direction ** 2, dim=2)
+    ray_to_sphere = torch.subtract(ray_origin, sphere_pos)
+    b = 2 * torch.sum(ray_to_sphere * ray_direction, dim=2)
+    c = torch.sum(ray_to_sphere ** 2, dim=2) - sphere_radius ** 2
+    hit = b ** 2 - 4 * a * c >= 0
+    q = - b + torch.sign(b) * torch.sqrt(b ** 2 * 4 * a * c) / 2
 
-    discriminant = b ** 2 - 4 * a * c
+    t0 = q / a
+    t1 = c / q
 
-    print(discriminant)
+    return hit, ray_direction * t0 + ray_origin, ray_direction * t1 + ray_origin
 
-    return discriminant >= 0
-
-image = ray_sphere_intersect(rays, sphere).numpy()
-image = image.astype(float)
+hit_mask, p0, p1 = ray_sphere_intersect(ray_origin, ray_direction, sphere_pos, sphere_radius).numpy()
+image = hit_mask.astype(float)
 print(image)
 print(image.shape)
 print(image.dtype)
