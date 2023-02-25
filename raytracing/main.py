8d0f5d27c7c9239f032705242e7af64687dce5dc
andrinr <andrinrehmann@gmail.com>
2023-02-25 21:05:37 +0100

    more abstraction

--- raytracing/main.py
+++ raytracing/main.py
@@ -1,30 +1,27 @@
 import torch
 import matplotlib.pyplot as plt
+from trace import Rays, Light, Sphere, Tracer
 
-from trace import Rays, Light, Sphere
-
-frustum_resolution = (7000, 7000)
+frustum_resolution = (1000, 1000)
 
 rays = Rays.create(torch.tensor([0, 0, 0], dtype=torch.float32), frustum_resolution)
+light = Light([2, 1, 0])
 
-light = Light(torch.tensor([2, 1, 0], dtype=torch.float32))
-sphere = Sphere(torch.tensor([0, 0, 3], dtype=torch.float32), torch.tensor([1], dtype=torch.float32))
-sphee2 = Sphere(torch.tensor([0, 1, 3], dtype=torch.float32), torch.tensor([0.5], dtype=torch.float32))
+spheres = []
+spheres.append(Sphere([0, 0, 3], [1], [1.0, 0.0, 0.0]))
+spheres.append(Sphere([0, 1, 2], [0.2], [0.0, 0.0, 1.0]))
 
 # gpu acceleration
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-rays.to(device)
-light.to(device)
-sphere.to(device)
 
-intersection_point, ray_direction_shadow, mask_object = sphere.intersect(rays, light)
+tracer = Tracer(spheres, light, device)
 
-# free memory of rays
-del rays
-rays = Rays(intersection_point, ray_direction_shadow)
-_, _, mask_light = sphere.intersect(rays, light)
+hits_object = tracer(rays)
+light_blocked = tracer(rays)
 
-image = mask_light * mask_object
+print(hits_object.shape)
+
+image = hits_object & ~light_blocked
 image = image.cpu().numpy()
 
 plt.imshow(image)
--- raytracing/trace.py
+++ raytracing/trace.py
@@ -1,11 +1,13 @@
 import torch
+import typing
 
 class Rays:
-    def __init__(self, origin, direction):
+    def __init__(self, origin : torch.TensorType, direction : torch.TensorType):
         self.origin = origin
         self.direction = direction
+        self.color = torch.ones_like(self.direction)
 
-    def create(origin, resolution, distance = 1):
+    def create(origin, resolution : tuple, distance = 1):
         frustum = torch.ones((resolution[0], resolution[1], 3), dtype=torch.float32)
         frustum[..., 0] = torch.linspace(-1, 1, resolution[0]).view(-1, 1)
         frustum[..., 1] = torch.linspace(-1, 1, resolution[1]).view(1, -1)
@@ -20,46 +22,79 @@ class Rays:
     def to(self, device):
         self.origin = self.origin.to(device)
         self.direction = self.direction.to(device)
+        return self
 
 class Light:
-    def __init__(self, pos):
-        self.pos = pos
+    def __init__(self, pos : list):
+        self.pos = torch.tensor(pos, dtype=torch.float32)
 
     def to(self, device):
         self.pos = self.pos.to(device)
+        return self
 
 class Sphere:
-    def __init__(self, pos, radius):
-        self.pos = pos
-        self.radius = radius
+    def __init__(self, 
+        pos : list, 
+        radius : list,
+        color : list):
+        self.pos = torch.tensor(pos, dtype=torch.float32)
+        self.radius = torch.tensor(radius, dtype=torch.float32)
+        self.color = torch.tensor(color, dtype=torch.float32)
 
     def to(self, device):
         self.pos = self.pos.to(device)
         self.radius = self.radius.to(device)
+        self.color = self.color.to(device)
+        return self
 
-    def intersect(self, rays : Rays, light : Light):
-        a = torch.sum(rays.direction ** 2, dim=2)
-        ray_to_sphere = torch.subtract(rays.origin, self.pos)
-        b = 2 * torch.sum(ray_to_sphere * rays.direction, dim=2)
-        c = torch.sum(ray_to_sphere ** 2, dim=2) - self.radius ** 2
-        disc = b ** 2 - 4 * a * c
-        mask = disc >= 0
-        mask = mask.unsqueeze(2)
-        
-        q = - (b + torch.sign(b) * torch.sqrt(disc)) / 2
-        t0 = torch.div(q, a).unsqueeze(2)
-        t1 = torch.div(c, q).unsqueeze(2)
-        t = torch.min(t0, t1)
+class Tracer:
+    def __init__(
+            self, 
+            spheres : list[Sphere], 
+            light : Light, 
+            device : str,
+            tolerance : float = 0.001,
+            maxDistance : float = 1000) -> None:
 
-        mask = mask & (torch.abs(t) > 0.001)
-        intersection_point = torch.mul(t, rays.direction) + rays.origin
+        self.device = device
+        self.spheres = spheres
+        for sphere in spheres:
+            sphere = sphere.to(device)
 
-        #normals = intersection_point - sphere_pos
-        #normals = torch.div(normals, torch.norm(normals, dim=2, keepdim=True))
+        self.light = light.to(device)
+        self.tolerance = tolerance
+        self.maxDistance = maxDistance
 
-        ray_direction_shadow = light.pos - intersection_point
-        ray_direction_shadow = torch.div(ray_direction_shadow, torch.norm(ray_direction_shadow, dim=2, keepdim=True))
+    def __call__(self, rays : Rays):
+        """
+        GPU ray tracer
+        """
 
-        return intersection_point, ray_direction_shadow, mask
+        # find the minimal intersection point
+        rays = rays.to(self.device)
+        t = torch.full(rays.direction.shape[0:2], self.maxDistance).to(self.device)
 
+        for sphere in self.spheres:
+            a = torch.sum(rays.direction ** 2, dim=2)
+            ray_to_sphere = torch.subtract(rays.origin, sphere.pos)
+            b = 2 * torch.sum(ray_to_sphere * rays.direction, dim=2)
+            c = torch.sum(ray_to_sphere ** 2, dim=2) - sphere.radius ** 2
+            disc = b ** 2 - 4 * a * c
+            mask = disc >= 0
+            mask = mask.unsqueeze(2) 
+            
+            q = - (b + torch.sign(b) * torch.sqrt(disc)) / 2
+            t_prime = torch.div(q, a).unsqueeze(2)
+            t_prime = torch.min(torch.div(c, q).unsqueeze(2), t_prime)
+            t_prime[~mask] = self.maxDistance
+            t = torch.min(t, t_prime)
 
+        update = t < self.maxDistance
+        rays.origin[update] = (torch.mul(t, rays.direction) + rays.origin)[update]
+        rays.direction[update] = (self.light.pos - rays.origin)[update]
+        # slight offset to avoid hitting same surface again
+        rays.origin += self.tolerance * rays.direction
+        # normalize directions
+        rays.direction = torch.div(rays.direction, torch.norm(rays.direction, dim=2, keepdim=True))
+
+        return update
