50b2edaa3467a82b26be211a8fba66f9d7424cd4
andrinr <andrinrehmann@gmail.com>
2023-04-11 15:55:04 +0200

    continue multigrid, unsure about prolongation method

--- multigrid/grid/grid.py
+++ multigrid/grid/grid.py
@@ -6,21 +6,17 @@ class Multigrid:
 
     def __init__(self, 
             stencil : np.ndarray, 
-            b : np.ndarray, 
-            recurse_kernel : np.ndarray,
+            phi : np.ndarray, 
+            restrict_kernel : np.ndarray,
             project_kernel : np.ndarray,
             schema : str = "rrpp",
             pre_smoothing_steps : int = 16):
-        self.L = stencil
-        self.b = b
-        if self.b.ndim != 1:
-            # row major flattening
-            self.b = self.b.flatten()
-
+        self.stencil = stencil
+        self.phi = phi
         self.pre_smoothing_steps = pre_smoothing_steps
-        self.recurse_kernel = recurse_kernel
+        self.restrict_kernel = restrict_kernel
         self.project_kernel = project_kernel
-        self.parse_schema(schema=schema)
+        self.max_depth = self.parse_schema(schema=schema)
 
     def solve(self):
         # We want to find x such that Ax = b
@@ -28,27 +24,40 @@ class Multigrid:
         # Instead we can use multigrid to solve this problem
 
         # initial guess
-        x = self.b
+        x = self.phi
 
         # pre smoothing
         x = self.relax(x, self.pre_smoothing_steps)
 
+        d = self.defect(self.stencil, self.phi, x)
+
         # coarse grid correction
         for map in self.schema:
             if map == "r":
-                x = self.restrict(x)
+                d = self.restrict(d)
+                eps = 
+                self.relax
             elif map == "p":
                 x = self.prolongate(x)
-            else:
-                raise Exception("Invalid map")
         
         return x
     
+    def recurse(self, x : np.ndarray, depth : int) -> np.ndarray:
+        if depth == self.max_depth:
+            return self.relax(x, self.pre_smoothing_steps)
+        else:
+            x = self.relax(x, self.pre_smoothing_steps)
+            d = self.defect(self.stencil, self.phi, x)
+            d = self.restrict(d)
+            x = self.recurse(x, depth + 1)
+            x = self.prolongate(x)
+            x = self.relax(x, self.pre_smoothing_steps)
+            return x
+    
     def relax(self, x : np.ndarray, steps : int) -> np.ndarray:
-        # iterative improvement
-        for i in range(steps):
-            correction = solver.Seidel(A, -self.defect(x)).solve()
-            x = x + correction
+        correction = solver.StencilJacobi(
+            self.stencil, -self.defect(x), max_iterations=steps).solve()
+        x = x + correction
 
         return x
     
@@ -56,14 +65,15 @@ class Multigrid:
         return A @ x - b
     
     def restrict(self, x : np.ndarray) -> np.ndarray:
-        pass 
+        convolve(x, self.restrict_kernel, mode="constant")[::2, ::2]
 
     def prolongate(self, x : np.ndarray) -> np.ndarray:
-        pass
+        convolve(x, self.project_kernel, mode="constant")
     
-    def parse_schema(self, schema : str):
+    def parse_schema(self, schema : str) -> int:
         self.schema = [char for char in schema]
         assert all([char in ["r", "p"] for char in self.schema])
         assert schema.count("r") == schema.count("p")
         assert self.schema[0] == "r"
         assert self.schema[-1] == "p"
+        return len(self.schema) // 2
--- multigrid/grid/helpers.py
+++ multigrid/grid/helpers.py
@@ -1,14 +1,23 @@
 import numpy as np
 
-def build_matrix_from_kernel(kernel : np.ndarray, dim : int) -> np.ndarray:
-    A = np.zeros((dim, dim))
+def sample_galaxy(rate = 256, n_galaxies = 20, n_particles_per_galaxy=2000, seed=0):
+    np.random.seed(seed)
+    grid = np.zeros((rate, rate))
 
-    row = np.zeros(dim)
-    row[0] = kernel[0, 1]
-    row[1] = kernel[0, 2]
-    row[dim - 2] = kernel[0, 0]
+    particles = np.zeros((n_particles_per_galaxy * n_galaxies, 2))
+    for i in range(n_galaxies):
+        galaxy = np.random.normal(0, np.random.rand()*0.1+0.05, (n_particles_per_galaxy,2))
+        galaxy += np.random.rand(2)- 0.5
+        galaxy *= rate
+        galaxy += rate/2
+        particles[i*n_particles_per_galaxy : (i+1) * n_particles_per_galaxy] = galaxy
+    
+    # nearest grid point mass assignment
+    for particle in particles:
+        x = int(particle[0])
+        y = int(particle[1])
+        if x < 0 or x >= rate or y < 0 or y >= rate:
+            continue
+        grid[x, y] += 1
 
-    for i in range(dim):
-        A[i] = np.roll(row, i)
-
-    return A
+    return grid
